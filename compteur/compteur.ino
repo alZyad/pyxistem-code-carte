398b68086e670564f18fbb81d8fcf31bc835b9af
2019alalguiz <zyad.alalgui@student-cs.com>
2023-02-01 11:27:07 +0100

    add minute display to improve readability

--- compteur/compteur.ino
+++ compteur/compteur.ino
@@ -3,13 +3,24 @@ void setup() {
   Serial.begin(9600);
 
 }
-  int i = 0;
+  int secondes = 0;
+  int minutes = 0;
+  int secondes_restantes = 0; 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("\ncela fait ");
-  Serial.print(i);
-  Serial.print(" secondes que le programme a ete lance.\n");
+  if(secondes < 60){
+    Serial.print(secondes);
+    Serial.print(" secondes que le programme a ete lance.\n");
+  } else {
+    minutes = secondes / 60;
+    secondes_restantes = secondes - minutes * 60;
+    Serial.print(minutes);
+    Serial.print(" minutes et ");
+    Serial.print(secondes_restantes);
+    Serial.print(" secondes que le programme a ete lance.\n");
+  }
   delay(1000);
-  i = i + 1;
+  secondes = secondes + 1;
   
 }
