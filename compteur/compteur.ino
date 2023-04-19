8cbd283a09f6b4ce48720b0b0434d25dcfd377d7
2019alalguiz <zyad.alalgui@student-cs.com>
2023-04-19 04:33:23 +0200

    add code to output fqbn of electronic card

--- compteur/compteur.ino
+++ compteur/compteur.ino
@@ -1,3 +1,30 @@
+#if defined(ESP32)
+#define board_fqbn "esp32:esp32:esp32"
+#elif defined(ARDUINO_ARDUINO_NANO33BLE)
+#define board_fqbn "arduino:mbed_nano:nano33ble"
+#else
+#error "This board is not recognized"
+#endif
+
+void show_fqbn() {
+  int command_size = 10;
+  char command_char[command_size] = "";
+  String command = "";
+  int availableBytes = Serial.available();
+  for(int i=0; i<min(availableBytes, command_size); i++)
+  {
+      command_char[i] = Serial.read();
+      
+  }
+  command = String(command_char).substring(0,strlen(command_char)-1);
+  if(command == "fqbn") {
+    Serial.print("\n");
+    Serial.print(board_fqbn);
+    Serial.print("\n");
+
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,6 +35,7 @@ void setup() {
   int secondes_restantes = 0; 
 void loop() {
   // put your main code here, to run repeatedly:
+  show_fqbn();
   Serial.print("\ncela fait ");
   if(secondes < 60){
     Serial.print(secondes);
